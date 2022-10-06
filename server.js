8169a1f0355bf1bd8d9956b832f1f75f66d352ba
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2022-10-06 15:29:50 +0200

    .

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     // console.log the bearer token
+    console.log('bearer')
     console.log(req.headers.authorization);
+    console.log('req', req)
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 }
 );
 app.listen(port, ip, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`test Example app listening at http://localhost:${port}`)
 }
 );
